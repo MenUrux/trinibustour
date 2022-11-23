2aabfbb5d900cac5a57cf5661868ffe581f5c2b9
Menurux <jorgepino.utu@gmail.com>
2022-11-23 04:25:48 -0300

    Intento de hacer flip tours

--- js/tours.js
+++ js/tours.js
@@ -1,4 +1,29 @@
-var container = document.getElementById("tour-container");
+document.getElementById("rutaroja").onclick = function() {myFunction()};
 
-container.appendChild.apply('<div>Sadasd<div>')
 
+rutaRoja = document.getElementById("roja");
+cardsContainer = document.getElementById("cards-container");
+
+let rojo = '#d7263dff';
+
+
+function myFunction() {
+
+    
+    if (rutaRoja.style.display = 'none'){
+        
+        rutaRoja.style.display = 'flex';
+        cardsContainer.style.display = 'none';
+
+        rutaRoja.style.transition = "all 1s"
+
+        setTimeout(() => {
+            rutaRoja.style.backgroundColor = rojo;
+          })
+
+
+        console.log('ruta ' + rutaRoja.style.display);
+        console.log('Cards ' + cardsContainer.style.display);
+    }
+
+}
