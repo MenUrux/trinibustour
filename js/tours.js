2c324291ec179320fc906b5c60f44be7b3eef6c5
Menurux <jorgepino.utu@gmail.com>
2022-11-23 18:47:46 -0300

    nuevos arreglos 3

--- js/tours.js
+++ js/tours.js
@@ -1,21 +1,29 @@
 
 
-document.getElementById("rutaroja").onclick = function() {myFunction()};
+document.getElementById("rutaroja").onclick = function() {rutaRojaFunction()};
+document.getElementById("rutaverde").onclick = function() {rutaVerdeFunction()};
+
 
 tourTodo = document.getElementById("tour-todo")
 rutaRoja = document.getElementById("roja");
+rutaVerde = document.getElementById("verde");
 cardsContainer = document.getElementById("cards-container");
+
+horarioPartidaRojo = document.getElementById('horario-partida-rojo');
+horarioPartidaVerde = document.getElementById('horario-partida-verde');
+
 
 let rojo = '#d7263dff';
 
 
-function myFunction() {
+function rutaRojaFunction() {
 
     
     if (rutaRoja.style.display = 'none'){
         
         rutaRoja.style.display = 'flex';
         cardsContainer.style.display = 'none';
+        rutaVerde.style.display = 'none';
 
         rutaRoja.style.transition = "all 1s"
         tourTodo.style.transition.backgroundColor = "all 1s"
@@ -32,4 +40,42 @@ function myFunction() {
         console.log('Cards ' + cardsContainer.style.display);
     }
 
+}
+
+function rutaVerdeFunction() {
+
+    
+    if (rutaVerde.style.display = 'none'){
+        
+        rutaVerde.style.display = 'flex';
+        cardsContainer.style.display = 'none';
+        rutaRoja.style.display = 'none';
+
+        rutaVerde.style.transition = "all 1s"
+        tourTodo.style.transition.backgroundColor = "all 1s"
+
+        setTimeout(() => {
+            // rutaRoja.style.backgroundColor = rojo;
+            tourTodo.style.background = 'linear-gradient(-45deg, #00AA00, #00ff00, #00AA00, #00AA00';
+          })
+
+          
+
+
+        console.log('ruta ' + rutaVerde.style.display);
+        console.log('Cards ' + cardsContainer.style.display);
+    }
+
+}
+
+var primersalida = Boolean (true);
+primersalida = true;
+
+if (primersalida == true){
+    horarioPartidaRojo.textContent = '9:30';
+    horarioPartidaVerde.textContent = '9:30';
+}
+else{
+    horarioPartidaRojo.textContent = '12:30';
+    horarioPartidaVerde.textContent = '12:30';
 }
