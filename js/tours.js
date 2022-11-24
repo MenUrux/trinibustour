e4fda2728bdf682471f22ea69908b40c344b64fa
Menurux <jorgepino.utu@gmail.com>
2022-11-24 04:18:32 -0300

    turnos hechos

--- js/tours.js
+++ js/tours.js
@@ -1,7 +1,7 @@
 
 
-document.getElementById("rutaroja").onclick = function() {rutaRojaFunction()};
-document.getElementById("rutaverde").onclick = function() {rutaVerdeFunction()};
+document.getElementById("rutaroja").onclick = function () { rutaRojaFunction() };
+document.getElementById("rutaverde").onclick = function () { rutaVerdeFunction() };
 
 
 tourTodo = document.getElementById("tour-todo")
@@ -10,17 +10,24 @@ rutaVerde = document.getElementById("verde");
 cardsContainer = document.getElementById("cards-container");
 
 horarioPartidaRojo = document.getElementById('horario-partida-rojo');
+horarioParadaUnoRojo = document.getElementById('horario-parada-uno-rojo');
+horarioParadaDosRojo = document.getElementById('horario-parada-dos-rojo');
+horarioParadaTresRojo = document.getElementById('horario-parada-tres-rojo');
+horarioFinalRojo = document.getElementById('horario-final-rojo');
+
 horarioPartidaVerde = document.getElementById('horario-partida-verde');
-
-
-let rojo = '#d7263dff';
+horarioParadaUnoVerde = document.getElementById('horario-parada-uno-verde');
+horarioParadaDosVerde = document.getElementById('horario-parada-dos-verde');
+horarioParadaTresVerde = document.getElementById('horario-parada-tres-verde');
+horarioParadaCuatroVerde = document.getElementById('horario-parada-cuatro-verde');
+horarioFinalVerde = document.getElementById('horario-final-verde');
 
 
 function rutaRojaFunction() {
 
-    
-    if (rutaRoja.style.display = 'none'){
-        
+
+    if (rutaRoja.style.display = 'none') {
+
         rutaRoja.style.display = 'flex';
         cardsContainer.style.display = 'none';
         rutaVerde.style.display = 'none';
@@ -29,11 +36,10 @@ function rutaRojaFunction() {
         tourTodo.style.transition.backgroundColor = "all 1s"
 
         setTimeout(() => {
-            // rutaRoja.style.backgroundColor = rojo;
             tourTodo.style.background = 'linear-gradient(-45deg, #a32233, #a32233, #d7263dff, #a32233';
-          })
+        })
 
-          
+
 
 
         console.log('ruta ' + rutaRoja.style.display);
@@ -44,9 +50,9 @@ function rutaRojaFunction() {
 
 function rutaVerdeFunction() {
 
-    
-    if (rutaVerde.style.display = 'none'){
-        
+
+    if (rutaVerde.style.display = 'none') {
+
         rutaVerde.style.display = 'flex';
         cardsContainer.style.display = 'none';
         rutaRoja.style.display = 'none';
@@ -57,9 +63,9 @@ function rutaVerdeFunction() {
         setTimeout(() => {
             // rutaRoja.style.backgroundColor = rojo;
             tourTodo.style.background = 'linear-gradient(-45deg, #00AA00, #00ff00, #00AA00, #00AA00';
-          })
+        })
 
-          
+
 
 
         console.log('ruta ' + rutaVerde.style.display);
@@ -68,14 +74,108 @@ function rutaVerdeFunction() {
 
 }
 
-var primersalida = Boolean (true);
-primersalida = true;
 
-if (primersalida == true){
-    horarioPartidaRojo.textContent = '9:30';
-    horarioPartidaVerde.textContent = '9:30';
+var rad = document.turnoForm.turno;
+var prev = null;
+var turnoUno = true;
+for (var i = 0; i < rad.length; i++) {
+    rad[i].addEventListener('change', function () {
+        (prev) ? console.log(prev.value) : null;
+        if (this !== prev) {
+            prev = this;
+        }
+        console.log('valor ' +this.value)
+
+        if (this.value == 'turnoUno') {
+            horarioPartidaRojo.textContent = '9:30';
+            horarioParadaUnoRojo.textContent = '10:00';
+            horarioParadaDosRojo.textContent = '11:00';
+            horarioParadaTresRojo.textContent = '11:30';
+            horarioFinalRojo.textContent = '12:00'
+            
+
+
+            horarioPartidaVerde.textContent = '9:30';
+            horarioParadaUnoVerde.textContent = '9:45';
+            horarioParadaDosVerde.textContent = '10:15';
+            horarioParadaTresVerde.textContent = '10:35';
+            horarioParadaCuatroVerde.textContent = '11:30';
+            horarioFinalVerde.textContent = '12:00'
+        }
+        else if (this.value == 'turnoDos') {
+            horarioPartidaRojo.textContent = '12:30';
+            horarioParadaUnoRojo.textContent = '13:00';
+            horarioParadaDosRojo.textContent = '13:30';
+            horarioParadaTresRojo.textContent = '13:55';
+            horarioFinalRojo.textContent = '14:25'
+
+
+            horarioPartidaVerde.textContent = '14:00';
+            horarioParadaUnoVerde.textContent = '14:15';
+            horarioParadaDosVerde.textContent = '14:45';
+            horarioParadaTresVerde.textContent = '15:05';
+            horarioParadaCuatroVerde.textContent = '15:35';
+            horarioFinalVerde.textContent = '15:55'
+        }
+
+    });
 }
-else{
-    horarioPartidaRojo.textContent = '12:30';
-    horarioPartidaVerde.textContent = '12b:30';
+
+var radVerde = document.turnoVerdeForm.turno;
+var prevVerde = null;
+for (var i = 0; i < radVerde.length; i++) {
+    radVerde[i].addEventListener('change', function () {
+        (prevVerde) ? console.log(prevVerde.value) : null;
+        if (this !== prevVerde) {
+            prevVerde = this;
+        }
+        console.log('valor ' +this.value)
+
+        if (this.value == 'turnoUno') {
+            horarioPartidaVerde.textContent = '9:30';
+            horarioParadaUnoVerde.textContent = '9:45';
+            horarioParadaDosVerde.textContent = '10:15';
+            horarioParadaTresVerde.textContent = '10:35';
+            horarioParadaCuatroVerde.textContent = '11:30';
+            horarioFinalVerde.textContent = '12:00'
+        }
+        else if (this.value == 'turnoDos') {
+            horarioPartidaVerde.textContent = '14:00';
+            horarioParadaUnoVerde.textContent = '14:15';
+            horarioParadaDosVerde.textContent = '14:45';
+            horarioParadaTresVerde.textContent = '15:05';
+            horarioParadaCuatroVerde.textContent = '15:35';
+            horarioFinalVerde.textContent = '15:55'
+        }
+
+    });
 }
+
+
+// let seleccionarturno = document.getElementById('seleccionarxd');
+// seleccionarturno.addEventListener('click', () => {
+//     let selected = document.querySelector('input[type="radio"]:checked');
+
+//     let turno = String(selected.value);
+//     console.log(turno);
+//     if (turno == 'turnoUno') {
+//         console.log('seleccionado');
+//         horarioPartidaRojo.textContent = '9:30';
+//         horarioPartidaVerde.textContent = '9:30';
+//     }
+//     else if (turno == 'turnoDos') {
+//         console.log('selecc');
+//         horarioPartidaRojo.textContent = '12:30';
+//         horarioPartidaVerde.textContent = '12:30';
+//     }
+
+// })
+
+
+// document.getElementById("seleccionarxd").onclick = function() {mostrarTurno()};
+
+
+// function mostrarTurno(){
+//     console.log('dentro 1 ' +turnoUno)
+// console.log('dentro 2 ' +turnoDos)
+
